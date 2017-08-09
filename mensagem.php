38d9d829685ebaed9eed4c2325b7d916dd8a8be2
flaviolages <meflaviolages@gmail.com>
2017-08-09 15:42:10 -0200

    Update mensagem.php

--- mensagem.php
+++ mensagem.php
@@ -3,19 +3,19 @@ ini_set("error_reporting", E_ALL);
 // O remetente deve ser um e-mail do seu dom�nio conforme determina a RFC 822.
 // O return-path deve ser ser o mesmo e-mail do remetente.
 
-$nome = $_POST["nome"];
-$telefone= $_POST["telefone"];
-$email = $_POST["email"];
-$mensagem = $_POST["mensagem"];
+$n = $_POST["nome"];
+$t = $_POST["telefone"];
+$e = $_POST["email"];
+$me = $_POST["mensagem"];
 $assunto="Contato pelo Site";
 
 
 $msg = "
 <font color=#6c8827 size=4 face=verdana><b>::: Mensagem enviada do site - Contato :::  </b></font><br><br>
-<font color=#6c8827 size=4 face=verdana><b>Nome: </b></font><font size=3> <font color=#333333 size=4 face=verdana>$nome</font><br>
-<font color=#6c8827 size=4 face=verdana><b>Email: </b></font> <font color=#333333 size=4 face=verdana>$email</font><br>
-<font color=#6c8827 size=4 face=verdana><b>Telefone:</b></font> <font color=#333333 size=4 face=verdana>$telefone</font></font><br>
-<font color=#6c8827 size=4 face=verdana><b>Mensagem:</b></font> <font color=#333333 size=4 face=verdana>$mensagem</font><br>
+<font color=#6c8827 size=4 face=verdana><b>Nome: </b></font><font size=3> <font color=#333333 size=4 face=verdana>$n</font><br>
+<font color=#6c8827 size=4 face=verdana><b>Email: </b></font> <font color=#333333 size=4 face=verdana>$e</font><br>
+<font color=#6c8827 size=4 face=verdana><b>Telefone:</b></font> <font color=#333333 size=4 face=verdana>$t</font></font><br>
+<font color=#6c8827 size=4 face=verdana><b>Mensagem:</b></font> <font color=#333333 size=4 face=verdana>$m</font><br>
 
 
 <br><br>";
@@ -44,6 +44,9 @@ $envio = mail($x, $assunto, $msg, $headers);
 
 
 if($envio)
+ echo $n;
+ echo $t;
+ echo $e;
  echo "<font color=#406980 size=4 face=verdana>
 
  <br><br><img src=https://logo.fretebras.com.br/507387972/transporte-gravatai-rs-ds-muller-transportes-e-logistica_g.jpg width=100 align=left hspace=20 vspace=30><br>
